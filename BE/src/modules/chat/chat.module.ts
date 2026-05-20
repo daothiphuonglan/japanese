e0d29c2fcc90b5dca9f4055f96bea3be4c569859
PhuongLan <daophuonglanlt@gmail.com>
2026-05-20 23:08:18 +0700

    [BE] Update chatModules

--- BE/src/modules/chat/chat.module.ts
+++ BE/src/modules/chat/chat.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatGateway } from '../../comon/gateways/chat.gateway';
+import { ChatController } from './chat.controller';
 import { PrismaService } from '../../database/prisma.service'; // Kiểm tra lại đường dẫn này
 
 @Module({
+  controllers: [ChatController],
   providers: [ChatService, ChatGateway, PrismaService],
   exports: [ChatService], // Nếu bạn muốn dùng ChatService ở các module khác
 })
