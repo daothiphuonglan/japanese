ccde12c65627e3762785b4e533bc567ca02a068b
PhuongLan <daophuonglanlt@gmail.com>
2026-07-12 01:53:36 +0700

    [FE][BE] Create guesting game

--- BE/src/app.module.ts
+++ BE/src/app.module.ts
@@ -7,12 +7,17 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './database/prisma.module';
 import { GatewayModule } from './comon/gateways/gateway.module';
+import { MatchmakingModule } from './modules/matchmaking/matchmaking.module';
 
 @Module({
-  imports: [KanaModule,  ConfigModule.forRoot({ isGlobal: true }),
+  imports: [
+    KanaModule,
+    ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
     PrismaModule,
-    GatewayModule,],
+    GatewayModule,
+    MatchmakingModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
